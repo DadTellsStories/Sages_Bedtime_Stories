6bc5f371be5d55c509e6d8ab8749291a4338c476
DadTellsStories <johnny.b.rockstar@gmail.com>
2025-02-19 15:35:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function loadStory() {
+    const storyContainer = document.getElementById("story-container");
+    storyContainer.innerHTML = "<h2>Tonight's Story</h2><p>Once upon a time, in a magical ocean, Sage and Mira the Mermaid...</p>";
+}
